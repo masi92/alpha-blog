66d458e955a6b95fa7f4c6e6badcf3fedee55d81
masi92 <39678756+masi92@users.noreply.github.com>
2019-08-21 10:03:49 +0300

    create users table and model

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'jquery-rails'
 
 gem 'slim-rails', '~> 3.1', '>= 3.1.1'
 
+gem "autoprefixer-rails"
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_112627) do
+ActiveRecord::Schema.define(version: 2019_08_21_065838) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
