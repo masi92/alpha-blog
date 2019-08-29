31b295db9d1ae7177b15443e52ef8acd182d8f69
masi92 <39678756+masi92@users.noreply.github.com>
2019-08-29 10:15:57 +0300

    Everything fixed all is good in the world : D (users controller & new artikkeli partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -18,24 +20,31 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             redirect_to articles_path, notice: "Your account was updated successfully"
-        end
-        else
         end
     end
 
     def show
-        @user = User.find(params[:id])
-        @user_articles = @user.articles.paginate(page: params[:page], per_parge: 5)
+        @user_articles = @user.articles.paginate(page: params[:page])
     end
 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:notice] = "You can only edit your own accout"
+            redirect_to root_path
+        end
+    end
+end
