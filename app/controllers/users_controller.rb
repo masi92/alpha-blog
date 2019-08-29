a134cbff16cdc57d786b4b654800ec2e99c408be
masi92 <39678756+masi92@users.noreply.github.com>
2019-08-29 10:24:07 +0300

    User signin on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to user_path(@user), notice: "welcome to the alpha blog #{@user.username}"
         else
             render 'new'
